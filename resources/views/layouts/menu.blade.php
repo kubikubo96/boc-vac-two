25b749bfad7cc765222c4bde18e86fcb68a60a58
TienNT <tien.nguyentat@evotek.vn>
2021-03-03 02:57:56 +0700

    "clean code"

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,9 +9,9 @@
         <a class="nav-link active" href="{{route('show.movingHouse')}}"><b id="menu4">CHUYỂN NHÀ TRỌN GÓI</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a class="nav-link active" href="{{route('show.contact')}}"><b id="menu5">LIÊN HỆ</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.contact')}}"><b id="menu5">LIÊN HỆ</b><span class="sr-only">(current)</span></a>
     </li>
 </ul>
