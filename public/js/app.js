c73175194f81d61f1da364a6d1789038b20e4a42
TienNT <tien.nguyentat@evotek.vn>
2021-03-03 02:49:03 +0700

    "clean code"

--- public/js/app.js
+++ public/js/app.js
@@ -63,17 +63,17 @@ function goTopFunc() {
 }
 
 if(window.location.pathname === '/about-us' || window.location.pathname === '/home' || window.location.pathname === '/') {
-    document.getElementById('menu1').style.borderBottom = '3px solid white';
+    document.getElementById('menu1').style.color = '#e53935';
 }
 if(window.location.pathname === '/news') {
-    document.getElementById('menu2').style.borderBottom = '3px solid white';
+    document.getElementById('menu2').style.color = '#e53935';
 }
 if(window.location.pathname === '/main_service') {
-    document.getElementById('menu3').style.borderBottom = '3px solid white';
+    document.getElementById('menu3').style.color = '#e53935';
 }
 if(window.location.pathname === '/moving_house') {
-    document.getElementById('menu4').style.borderBottom = '3px solid white';
+    document.getElementById('menu4').style.color = '#e53935';
 }
 if(window.location.pathname === '/contact') {
-    document.getElementById('menu5').style.borderBottom = '3px solid white';
+    document.getElementById('menu5').style.color = '#e53935';
 }
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,17 +1,17 @@
 <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-        <a id="menu1" class="nav-link" href="{{route('show.view')}}"><b>TRANG CHỦ</b> <span class="sr-only">(current)</span></a>
+        <a id="menu1" class="nav-link" href="{{route('show.view')}}"><b id="menu1">TRANG CHỦ</b> <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a id="menu2" class="nav-link active" href="{{route('show.news')}}"><b>TIN TỨC</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a id="menu3" class="nav-link active" href="{{route('show.mainService')}}"><b>DỊCH VỤ BỐC VÁC</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.mainService')}}"><b id="menu3">DỊCH VỤ BỐC VÁC</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a id="menu4" class="nav-link active" href="{{route('show.movingHouse')}}"><b>CHUYỂN NHÀ TRỌN GÓI</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.movingHouse')}}"><b id="menu4">CHUYỂN NHÀ TRỌN GÓI</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a id="menu5" class="nav-link active" href="{{route('show.contact')}}"><b>LIÊN HỆ</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.contact')}}"><b id="menu5">LIÊN HỆ</b><span class="sr-only">(current)</span></a>
     </li>
 </ul>
