eccd8720ea7b0d2e3330c95c44a2d64092d1d935
TienNT <tien.nguyentat@evotek.vn>
2021-03-03 02:53:03 +0700

    "clean code"

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,9 +3,6 @@
         <a id="menu1" class="nav-link" href="{{route('show.view')}}"><b id="menu1">TRANG CHỦ</b> <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item active">
         <a class="nav-link active" href="{{route('show.mainService')}}"><b id="menu3">DỊCH VỤ BỐC VÁC</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
@@ -13,5 +10,8 @@
     </li>
     <li class="nav-item active">
         <a class="nav-link active" href="{{route('show.contact')}}"><b id="menu5">LIÊN HỆ</b><span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
     </li>
 </ul>
