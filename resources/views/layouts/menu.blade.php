d932c188bebc29412c13c518fbbca8e5ca2e2d95
TienNT <tien.nguyentat@evotek.vn>
2021-03-06 22:21:13 +0700

    "change menu"

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,10 +9,10 @@
         <a class="nav-link active" href="{{route('show.movingHouse')}}"><b id="menu4">CHUYỂN NHÀ TRỌN GÓI</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
+        <a id="menu5" class="nav-link active" href="{{route('show.breakWall')}}"><b>ĐẬP PHÁ TƯỜNG</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
-        <a id="menu5" class="nav-link active" href="{{route('show.breakWall')}}"><b>ĐẬP PHÁ TƯỜNG</b><span class="sr-only">(current)</span></a>
+        <a class="nav-link active" href="{{route('show.news')}}"><b id="menu2">TIN TỨC</b><span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item active">
         <a id="menu6" class="nav-link active" href="{{route('show.contact')}}"><b>LIÊN HỆ</b><span class="sr-only">(current)</span></a>
